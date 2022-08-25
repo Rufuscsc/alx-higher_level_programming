05cd79f71306b1b272fc87231a02f621410f86fc
Rufus wells <rufusmfmwellens@gmail.com>
2022-08-25 21:43:11 +0100

    inital commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 for ch in reversed(range(97, 123)):
     print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
+
+#!/usr/bin/python3
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
